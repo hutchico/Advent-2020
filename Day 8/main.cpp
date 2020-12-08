dcfaea6c9bdf3b0be286ea082e9c856b1648779e
hutchico <hutchico@oregonstate.edu>
2020-12-08 00:08:19 -0800

    Part 1 commit

---
+++ Day 8/main.cpp
@@ -0,0 +1,92 @@
+#include <iostream>
+#include <fstream>
+#include <vector>
+#include <sstream>
+
+using std::vector;
+using std::string;
+using std::cout;
+using std::endl;
+
+int main()
+{
+    std::ifstream file;
+    std::stringstream ss;
+    vector<string> commands;
+    vector<int> runCount;
+    string buffer;
+    int acc = 0;
+    int next_instruction = 0;
+    int current_instruction = 0;
+
+    file.open("input.txt");
+    while(true){
+        getline(file,buffer);
+        if(buffer == "")
+            break;
+        commands.push_back(buffer);
+    }
+    runCount.resize(commands.size(),0);
+    //cout << commands[0];
+    while(true){
+        //Run current command
+        current_instruction = next_instruction;
+        cout << current_instruction << " " << commands[current_instruction][0] << " " << commands[current_instruction][4] << endl;
+        std::stringstream().swap(ss); //Reset stream
+        ss.str(commands[current_instruction]);
+        if(commands[current_instruction][0] == 'a'){ //Acc
+            next_instruction = current_instruction + 1;
+            if(commands[current_instruction][4] == '-'){
+                getline(ss,buffer,'-');
+                getline(ss,buffer);
+                acc -= stoi(buffer);
+            }
+            else{
+                getline(ss,buffer,'+');
+                getline(ss,buffer);
+                acc += stoi(buffer);
+            }
+            runCount[current_instruction]++;
+            next_instruction = current_instruction + 1;
+        }
+        else if(commands[current_instruction][0] == 'j'){ //Jmp
+            if(commands[current_instruction][4] == '-'){
+                getline(ss,buffer,'-');
+                getline(ss,buffer);
+                next_instruction = current_instruction - stoi(buffer);
+                runCount[current_instruction]++;
+            }
+            else{
+                getline(ss,buffer,'+');
+                getline(ss,buffer);
+                next_instruction = current_instruction + stoi(buffer);
+                runCount[current_instruction]++;
+            }
+        }
+        else{   //Nop
+            next_instruction = current_instruction + 1;
+        }
+
+
+        if(runCount[next_instruction] == 1){    //The next instruction to be run is going to be the duplicate we are looking for
+            cout << acc << endl;
+            break;
+        }
+    }
+
+    //Now to reverse the code starting from the end to determine where
+    while(true){
+        current_instruction = commands.size() - 1;
+        for(int i = commands.size() - 1; i == 0; i--){
+            std::stringstream().swap(ss);
+            ss.str(commands[i]);
+            getline(ss,buffer,' ');
+            if(commands[i][0] != 'j')
+                continue;
+            else
+
+        }
+    }
+
+    return 0;
+}
